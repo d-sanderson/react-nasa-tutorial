37575a558ad5e44b54834cc06a1534f729a7df70
dsanderson90 <dsanderson3@cnm.edu>
2018-12-26 22:37:13 -0700

    fixed some formatting

--- src/Grid/GridItem.js
+++ src/Grid/GridItem.js
@@ -6,7 +6,9 @@ import Explorer from './Explorer';
 const GridItem = (props) => {
 	return (
 		<div className={styles.card} onClick={() => props.updateClicked(props.fact.id) }>
+			<div className={styles.img}>
 			{props.fact.clicked ? <Explorer /> : <Badge />}
+			</div>
 			<h2 className={styles.title}>{props.fact.title}</h2>
 			<article className={styles.fact}>{props.fact.fact}</article>
 		</div>
